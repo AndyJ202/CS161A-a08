84dae19079414bcf48e25877b125cb277e0c9a5e
AndyJ202 <andy.joy@pcc.edu>
2026-06-08 08:22:23 +0000

    Complete

--- main.cpp
+++ main.cpp
@@ -1,80 +1,156 @@
-// ------------- FILE HEADER -------------
-// Author ✅: 
-// Assignment ✅:
-// Date ✅:
-// Citations: 
-
-
-// ------------- ZYBOOKS SCORES -------------
-// Chapter ✅: 
-// Participation ✅: 
-// Challenge ✅:
-// Labs ✅:
-
-
-// ------------- DISCORD POSTS -------------
-// https://discord.com/invite/URYKKf8YHm
-// Count ✅:
-// Links (Optional): 
-
-
-// ------------- DESIGN DOCUMENT -------------
-// A. INPUT ✅: 
-// B. OUTPUT ✅:
-// C. CALCULATIONS ✅:
-// D. LOGIC and ALGORITHMS ✅:
-//    (Optional) flow chart link or file name: 
-
-
-// ------------- TESTING -------------
-// PASS ALL GIVEN SAMPLE RUN TESTS ✅: 
-// (Optional) Additional tests count:   
-
-
-// ------------- CODE -------------
+/******************************************************************************
+# Author:           Andy Joy
+# Assignment:       Assignment 8
+# Date:             06/06/2026
+# Description:      This program calculates the final weighted numerical score 
+#                   and letter grade when given assignment, and exam scores. 
+#                   The program will only take 0-10 assignments using a 4 point 
+#                   grading scale.
+# Input:            userVal (int).
+# Output:           readScorePrompt (string), numericFinalScore (double), 
+#                   finalLetterGrade (char).
+# Sources:          N/A
+#******************************************************************************/
 #include <iostream>
-
+#include <iomanip>
+#include <cmath>
 using namespace std;
 
-// Function prototypes (if any)
+//Constants
+const double ASSINGMENTS_WEIGHT = 0.6;
+const double EXAMS_WEIGHT = 0.2;
 
+//Function prototypes
+void WelcomeMessage();
+int readInt(string prompt);
+void readScore(string prompt, double &num);
+double assignAverage(int numAssigns);
+void getInput(double &midtermScore, double &finalExamScore);
+double calcFinalScore(double assignAvg, double midterm, double final);
+void calcLetterGrade(double finalScore, char &letter);
 
-// Main function
-// https://en.cppreference.com/w/cpp/language/main_function.html
-int main(int argc, char* argv[]) {
-  cout << "Hello, World!" << endl;
+//Main function
+int main() {
+
+  int numAssigns = 0;
+  double assignAverageScore = 0.0;
+  double midtermExamScore = 0.0;
+  double finalExamScore = 0.0;
+  double numericFinalScore = 0.0;
+  char finalLetterGrade;
+
+  WelcomeMessage();
+
+  numAssigns = readInt("\nEnter the number of assignments (0 to 10): ");
+
+  while (numAssigns < 0 || numAssigns > 10) {
+    cout << "Illegal Value! Please try again!!" << endl;
+    numAssigns = readInt("Enter the number of assignments (0 to 10): ");
+  }
+  assignAverageScore = assignAverage(numAssigns);
+
+  getInput(midtermExamScore, finalExamScore);
+
+  cout << fixed << setprecision(1);
+  numericFinalScore = calcFinalScore(assignAverageScore, midtermExamScore, finalExamScore);
+  calcLetterGrade(numericFinalScore, finalLetterGrade);
+  cout << "\nYour Final Numeric score is " << numericFinalScore << endl;
+  cout << "Your Final Grade is " << finalLetterGrade << endl;
+  cout << "\nThank you for using my Grade Calculator!" << endl;
+
+  //End program
   return 0;
 }
 
-// Function implementations (if any)
+//Welcome Message
+void WelcomeMessage() {
+  cout << "Welcome to my Final Grade Calculator!" << endl;
+  cout << "Please enter the following information and I will calculate your Final Numerical Grade and Letter Grade for you!" << endl;
+  cout << "The number of assignments must be between 0 and 10." << endl;
+  cout << "All scores entered must be between 0 and 4." << endl;
+}
 
+//Integer input validation
+int readInt(string prompt){
+  int userVal = 0;
 
-// ------------- DESIGN -------------
-/* 
-Program Name:
+  cout << prompt;
+  cin >> userVal;
 
-Program Description:
+  while (!cin) {
+    cout << "Illegal Value! Please try again!!" << endl;
+    cin.clear();
+    cin.ignore(100, '\n');
+    cin >> userVal;
+  }
+  cin.ignore(50, '\n');
+  return userVal;
+}
 
-Design:
-A. INPUT
-Define the input variables including name data type. 
+//Integer input score validation. Like normal validation but also checks that valuue is 0-4 inclusive.
+void readScore(string prompt, double &num) {
+  cout << prompt;
+  cin >> num;
 
-B. OUTPUT
-Define the output variables including data types. 
+  while (!cin || num < 0 || num > 4) {
+    cout << "Illegal Score! Please try again!!" << endl;
+    cin.clear();
+    cin.ignore(100, '\n');
+    cout << prompt;
+    cin >> num;
+  }
+  cin.ignore(50, '\n');
+}
 
-C. CALCULATIONS
-Describe calculations used by algorithms in step D.  
-List all formulas. 
-If there are no calculations needed, state there are no calculations.
+//Reads assignment scores from the user. Then calculates average score.
+double assignAverage(int numAssigns) {
+  double totalAssignmentScore = 0.0;
+  double assignmentScore = 0.0;
+  double assignAverage = 0.0;
+  int i = 0;
+  string readScorePrompt;
 
-D. LOGIC and ALGORITHMS
-Design the logic of your program using pseudocode or flowcharts. 
-Use conditionals, loops, functions or array constructs.
-List the steps in transforming inputs into outputs. 
-https://github.com/Glen-Sasek-PCC-Instructor/2025-06-22/blob/main/Pseudocode-Reference.txt
+  for (i = 1; i <= numAssigns; ++i) {
+    readScorePrompt = "Enter score " + to_string(i) + ": ";
+    readScore (readScorePrompt, assignmentScore);
+    totalAssignmentScore += assignmentScore;
+  }
+  assignAverage = totalAssignmentScore / numAssigns;
 
+  return assignAverage;
+}
 
-SAMPLE RUNS
-Copy from assignment document.
+void getInput(double &midtermScore, double &finalExamScore) {
+  readScore("\nEnter your midterm exam score: ", midtermScore);
+  readScore("Enter your final exam score: ", finalExamScore);
+}
 
-*/
+//Takes assignment average score, midterm score, final exam score, and calculates final numerical score. Assignments weighted at 60%, midterm and final exams weighted at 20%.
+double calcFinalScore(double assignAvg, double midterm, double final) {
+  double finalScore = 0.0;
+
+  finalScore += assignAvg * ASSINGMENTS_WEIGHT;
+  finalScore += midterm * EXAMS_WEIGHT;
+  finalScore += final * EXAMS_WEIGHT;
+
+  return finalScore;
+}
+
+//Takes finalScore and returns letter grade.
+void calcLetterGrade(double finalScore, char &letter) {
+  if (finalScore >= 3.3 && finalScore <= 4.0){
+    letter = 'A';
+  }
+  else if (finalScore >= 2.8 && finalScore < 3.3) {
+    letter = 'B';
+  }
+  else if (finalScore >= 2.0 && finalScore < 2.8) {
+    letter = 'C';
+  }
+  else if (finalScore >= 1.2 && finalScore < 2.0) {
+    letter = 'D';
+  }
+  else {
+    letter = 'F';
+  }
+}
